903c7fc6c6539b51794c023e80cf66d30037267b
Ricardo Almeida da Rocha <qwertcardo@localhost.localdomain>
2020-07-04 09:47:53 -0300

    Criado formEvento.html

---
+++ src/main/java/com/qwertcardo/eventoapp/controllers/EventoController.java
@@ -0,0 +1,13 @@
+package com.qwertcardo.eventoapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+
+	@RequestMapping(value = "/cadastrarEvento")
+	public String form() {
+		return "evento/formEvento";
+	}
+}
--- src/main/java/com/qwertcardo/eventoapp/controllers/IndexController.java
+++ src/main/java/com/qwertcardo/eventoapp/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.qwertcardo.eventoapp;
+package com.qwertcardo.eventoapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
