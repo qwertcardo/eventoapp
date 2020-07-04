3f0167f1c5886cc0438e4857e465eeb55be5ec0f
Ricardo Almeida da Rocha <ricardoalmeida.for@gmail.com>
2020-07-04 11:58:57 -0300

    Criado metodo de salvar convidado

--- src/main/java/com/qwertcardo/eventoapp/controllers/EventoController.java
+++ src/main/java/com/qwertcardo/eventoapp/controllers/EventoController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.qwertcardo.eventoapp.models.Convidado;
 import com.qwertcardo.eventoapp.models.Evento;
+import com.qwertcardo.eventoapp.repository.ConvidadoRepository;
 import com.qwertcardo.eventoapp.repository.EventoRepository;
 
 @Controller
@@ -15,6 +17,8 @@ public class EventoController {
 
 	@Autowired
 	private EventoRepository er;
+	@Autowired
+	private ConvidadoRepository cr;
 
 	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
@@ -35,11 +39,20 @@ public class EventoController {
 		return mv;
 	}
 	
-	@RequestMapping(value = "/{codigo}")
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
 	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
 		Evento evento = er.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
 		mv.addObject("evento", evento);
 		return mv;
+	}
+	
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado) {
+		Evento evento = er.findByCodigo(codigo);
+		convidado.setEvento(evento);
+		cr.save(convidado);
+		
+		return "redirect:/{codigo}";
 	}
 }
---
+++ src/main/java/com/qwertcardo/eventoapp/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package com.qwertcardo.eventoapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.qwertcardo.eventoapp.models.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
+
+}
