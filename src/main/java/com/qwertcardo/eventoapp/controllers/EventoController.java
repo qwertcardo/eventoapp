be2e14f97a7e9d78838afb6b71b478963060f48e
Ricardo Almeida da Rocha <ricardoalmeida.for@gmail.com>
2020-07-04 22:36:33 -0300

    ajustado view convidado/evento

--- src/main/java/com/qwertcardo/eventoapp/controllers/EventoController.java
+++ src/main/java/com/qwertcardo/eventoapp/controllers/EventoController.java
@@ -44,6 +44,9 @@ public class EventoController {
 		Evento evento = er.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
 		mv.addObject("evento", evento);
+		
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
 		return mv;
 	}
 	
--- src/main/java/com/qwertcardo/eventoapp/repository/ConvidadoRepository.java
+++ src/main/java/com/qwertcardo/eventoapp/repository/ConvidadoRepository.java
@@ -3,7 +3,10 @@ package com.qwertcardo.eventoapp.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.qwertcardo.eventoapp.models.Convidado;
+import com.qwertcardo.eventoapp.models.Evento;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
+	
+	public Iterable<Convidado> findByEvento(Evento evento);
 
 }
